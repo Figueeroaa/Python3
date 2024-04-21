2a2d39d2d8964b8256a5225fed9f4efbaec32636
Martin <xmartin2007@gmail.com>
2024-04-21 02:06:29 -0400

    first commit

---
+++ descuento.py
@@ -0,0 +1,8 @@
+precio=int(input("Ingrese el precio:\n "))
+descuento=int(input("Ingresa un descuento:\n "))
+# se realiza descuento
+resultado=precio*descuento/100
+print(resultado)
+# descuento con el precio
+precio_final=precio-resultado
+print(precio_final)
